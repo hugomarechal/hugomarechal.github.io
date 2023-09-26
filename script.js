ce8048716356cb95ee2d46b2d1b89b0d5a3c69ca
Hugo Maréchal <132276877+hugomarechal@users.noreply.github.com>
2023-09-26 18:29:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showPopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'block';
+}
+
+function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
+
+window.onload = function () {
+    showPopup();
+};
+
